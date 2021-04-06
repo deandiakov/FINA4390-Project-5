4129b1ef638e3e0da97ccb3a95395fdb4caa695e
Zefeng (Daniel) Wang <wang.zef@husky.neu.edu>
2021-04-05 22:08:52 -0400

    retry push.

--- lending-app/pages/form.tsx
+++ lending-app/pages/form.tsx
@@ -1,13 +1,14 @@
 import Head from "next/head";
 import React, { useEffect } from "react";
 import { Input, InputNumber, Button } from "antd";
-import Router from "next/router";
+import Router, { useRouter } from "next/router";
 import "antd/dist/antd.css";
 
 export default function Form() {
+  const router = useRouter();
   const submitData = () => {
     // TODO: Make the axios call to the model, wait for the response then go to the results page
-    Router.push("/results");
+    router.push({ pathname: "/results", query: { approved: "denied" } });
   };
 
   return (
@@ -42,77 +43,6 @@ export default function Form() {
         </div>
       </main>
 
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 3rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-          flex-direction: column;
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   );
 }
--- lending-app/pages/results.tsx
+++ lending-app/pages/results.tsx
@@ -1,9 +1,20 @@
 import { Input } from "antd";
+import { withRouter, useRouter } from "next/router";
+import { useState } from "react";
 
 export default function Results() {
+  const router = useRouter();
+  const data = router.query;
+  const [approved, setApproved] = useState<string | string[]>(data.approved);
+
   return (
-    <div>
-      <h1>Results</h1>
+    <div className="my-container">
+      <h1 className="title">Based on your information, you have been:</h1>
+      {approved === "approved" ? (
+        <h1 className="title" style={{color:"green"}}>Approved</h1>
+      ) : (
+        <h1 className="title" style={{color:"red"}}>Denied</h1>
+      )}
     </div>
   );
 }
